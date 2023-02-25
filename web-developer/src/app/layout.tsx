e05db2a5f744869a1331345bf1d485c0e945aa02
Michael N. Jensen <michael@sys-adm.dk>
2023-02-25 03:43:51 +0100

    work in progress

---
+++ web-developer/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'web.developer',
+  description: 'web.developer - freelance development, freelance cloud consulting',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ web-developer/src/app/particles.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { loadFull } from "tsparticles";
+import Particles from "react-particles";
+import { useCallback } from "react";
+import { Engine } from "tsparticles-engine";
+import particlesPreset from "./particles-preset.json";
+
+export default function ParticlesComponent(props: { id: string }) {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        await loadFull(engine);
+    }, []);
+
+    return <Particles id={props.id} init={particlesInit} options={particlesPreset} />
+};
