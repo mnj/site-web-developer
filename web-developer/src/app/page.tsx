85a80834ae015d2c0d47b539ce82a07aa57fd4bf
Michael N. Jensen <michael@sys-adm.dk>
2023-02-25 16:17:57 +0100

    work in progress

--- web-developer/src/app/page.tsx
+++ web-developer/src/app/page.tsx
@@ -45,6 +45,15 @@ export default function Home() {
       </header>
 
       <section>
+        <div className="grid max-w-screen-lg px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28 bg-white dark:bg-gray-900">
+            <div className="mr-auto place-self-center lg:col-span-7">
+                <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">Building digital products & brands.</h1>
+                <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This free and open-source landing page template was built using the utility classes from <a href="https://tailwindcss.com" className="hover:underline">Tailwind CSS</a> and based on the components from the <a href="https://flowbite.com/docs/getting-started/introduction/" className="hover:underline">Flowbite Library</a> and the <a href="https://flowbite.com/blocks/" className="hover:underline">Blocks System</a>.</p>
+            </div>
+        </div>
+    </section>
+
+      <section>
         <div className="max-w-screen-lg mx-auto px-4 py-8 lg:px-6 lg:py-24 bg-gray-50 dark:bg-gray-800">
           <div className="w-full items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
             <div className="text-gray-500 sm:text-lg dark:text-gray-400">
